67ef9b8a0eb2d3b93a85e6597e3ca939e758ce82
cctulig <cctulig@gmail.com>
2022-01-23 15:21:00 -0500

    changed object detection model to yolo

--- src/camera/scripts/object_detection_model.py
+++ src/camera/scripts/object_detection_model.py
@@ -1,11 +1,12 @@
 import trash_item as ti
 import detectron2
 import numpy as np
+import torch
 
-import cv2
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
+# import cv2
+# from detectron2 import model_zoo
+# from detectron2.engine import DefaultPredictor
+# from detectron2.config import get_cfg
 
 # MAYBE NEED:
 # install dependencies: (use cu101 because colab has CUDA 10.1)
@@ -23,37 +24,68 @@ from detectron2.config import get_cfg
 class ObjectDetectionModel:
     
     def __init__(self):
-        weights_path = "weights/frcnn_labtrash_weights.pth"
-        cfg = get_cfg()
-        cfg.MODEL.DEVICE = "cpu"
-        cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
-        cfg.MODEL.WEIGHTS = weights_path
-        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5 #your number of classes + 1
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-        # cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.6
-        # cfg.MODEL.ROI_HEADS.IOU_THRESHOLDS = 0.6
-        self.predictor = DefaultPredictor(cfg)
+        #self.init_faster_rcnn()
+        self.init_yolo_v5()
 
         print("Model Init")
 
-    def classify(self, image):
+    # def init_faster_rcnn(self):
+    #     weights_path = "weights/frcnn_labtrash_weights.pth"
+    #     cfg = get_cfg()
+    #     cfg.MODEL.DEVICE = "cpu"
+    #     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
+    #     cfg.MODEL.WEIGHTS = weights_path
+    #     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5 #your number of classes + 1
+    #     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+    #     # cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.6
+    #     # cfg.MODEL.ROI_HEADS.IOU_THRESHOLDS = 0.6
+    #     self.predictor = DefaultPredictor(cfg)
 
-        outputs = self.predictor(image)
+    def init_yolo_v5(self):
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/yolo_lab_data_aug.pt')
+        #self.model = torch.hub.load('weights/custom_yolov5s.yaml', 'custom', path='weights/yolo_lab_data_aug.pt', source='local')
+
+    def classify(self, image):
+        return self.yolo_v5(image)
+        #return self.faster_rcnn(image)
+
+    # def faster_rcnn(self, image):
+    #     outputs = self.predictor(image)
+    #     predictions = []
+
+    #     boxes = outputs["instances"].get("pred_boxes")
+    #     classes = outputs["instances"].get("pred_classes").tolist()
+    #     scores = outputs["instances"].get("scores").tolist()
+
+    #     for i, box in enumerate(boxes):
+    #         XYXY_box = box.tolist()
+            
+    #         x = round((XYXY_box[2] + XYXY_box[0])/2) 
+    #         y = round((XYXY_box[3] + XYXY_box[1])/2) 
+    #         width = XYXY_box[2] - XYXY_box[0]
+    #         height = XYXY_box[3] - XYXY_box[1]
+            
+    #         new_trash_item = ti.TrashItem(x, y, width, height, classes[i], scores[i])
+    #         predictions.append(new_trash_item)
+
+    #     # print(predictions)
+    #     return predictions
+    
+    def yolo_v5(self, image):
+        results = self.model([image])
         predictions = []
 
-        boxes = outputs["instances"].get("pred_boxes")
-        classes = outputs["instances"].get("pred_classes").tolist()
-        scores = outputs["instances"].get("scores").tolist()
+        results.print()
+        print(results.pandas().xyxy)
+        print(results.pandas().xyxy[0])
 
-        for i, box in enumerate(boxes):
-            XYXY_box = box.tolist()
+        for i, trash in results.pandas().xyxy[0].iterrows():
+            x = round((int(trash[2]) + int(trash[0]))/2)
+            y = round((int(trash[3]) + int(trash[1]))/2)
+            width = int(trash[2]) - int(trash[0])
+            height = int(trash[3]) - int(trash[1])
             
-            x = XYXY_box[2] + round(XYXY_box[0]/2) 
-            y = XYXY_box[3] + round(XYXY_box[1]/2)
-            width = XYXY_box[2] - XYXY_box[0]
-            height = XYXY_box[3] - XYXY_box[1]
-            
-            new_trash_item = ti.TrashItem(x, y, width, height, classes[i], scores[i])
+            new_trash_item = ti.TrashItem(x, y, width, height, trash[5], float(trash[4]))
             predictions.append(new_trash_item)
 
         # print(predictions)
